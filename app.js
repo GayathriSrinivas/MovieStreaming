d5bbcead8a6a27ce6d660296be12ff6cec6abe94
Gayathri <gaya.0408@gmail.com>
2014-11-19 22:00:49 -0800

    html files

--- app.js
+++ app.js
@@ -6,13 +6,40 @@ var db = require("mongojs").connect(databaseUrl, collections);
 
 app.use(express.static(__dirname + '/static'));
 
+
 app.get('/movies', function (req, res) {
 	db.movie.find({} ,function(err,data){
 		console.log("-------------",data.length);
 		res.send(JSON.stringify({movies : data} ));
 
 	});
-})
+});
+
+app.get('/movies/:id', function(req,res){
+	var id = req.params.id;
+	var movie_details = {
+		videoUrl : "/raw_movie/" + id,
+		vidoeDesc : "Ice Age !!"
+	}
+	res.send(JSON.stringify({movie_details : movie_details}));
+	//res.send("Hello World ID ", id);
+});
+
+app.get('/raw_movie/:id', function(req,res){
+	var movieId = req.params.id;
+	var path = "/home/gayathri/movies/Ice Age (2002).mp4"
+	res.send(read(path));
+	db.movie.find({id: movieId} ,function(err,data){
+		console.log("-------------",data);
+		res.send(JSON.stringify({movies : data} ));
+
+	});
+});	
+
+app.get('/genre/findAll') {
+	return all the genre present 
+}
+
 
 var server = app.listen(3000, function () {
 
@@ -22,3 +49,4 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 })
+
--- static/client.js
+++ static/client.js
@@ -1,18 +1,34 @@
-$(function() {
 
+
+function getMovieList() {
 	$.get('/movies',function(data){
 		var movies = JSON.parse(data);
 		movies = movies.movies;
 		for (var i = 0 ; i<movies.length ; i++) {
 			var wstr = "";
 			wstr += "<li>";
-			wstr += '<a href="/movies/' + movies[i].id + '">'
+			wstr += '<a href="/movie_details.html#' + movies[i].id + '">'
 			wstr += movies[i].original_title;
 			wstr += '</a>';
 			wstr += "</li>";
 			$("#moviesList").append(wstr);
 		};
-
 	});
+}
 
-});
+function getMovieDetails() {
+	var id = window.location.href.split("#")[1];
+	var url = "/movies/" + id;
+	 
+	var videoUrl = '/videos/' + id + '.mp4';
+
+	$.get(url,function(data){
+		console.log("data", data);
+		var movies = JSON.parse(data);
+		movies = movies.movie_details;
+		$("#movieDesc").html(movies.vidoeDesc);
+		$("#movieVideo").html('<source src="' + videoUrl + '" type="video/mp4"></source>');
+		$('#movieVideo').get(0).play()
+		console.log("Dec....",movies.vidoeDesc)
+	});
+}
