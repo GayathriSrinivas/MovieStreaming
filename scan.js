50bf198e8315068ed2ea7943bdb86f3242c8618d
Gayathri <gaya.0408@gmail.com>
2014-11-22 14:43:53 -0800

    Functionality for Video Streaming

--- scan.js
+++ scan.js
@@ -1,9 +1,9 @@
 var mdb = require('moviedb')('fb92666a2288e824aaa575b983c6e182');
 var fs = require('fs');
-//var databaseUrl = "moviedb"; // "username:password@example.com/mydb"
 var databaseUrl = 'mongodb://localhost:27017/moviedb';
 var collections = ["movie","genre","translations","reviews"]
 var db = require("mongojs").connect(databaseUrl, collections);
+var exec = require('child_process').exec ;
 var folder;
 
 function getFolderPath() {
@@ -23,10 +23,10 @@ function printFileNamesInFolder(folderPath) {
 
 function readdirCallback(err, files) {
     if (err !== null) {
-        console.log(err);
+        //console.log(err);
     } else {
         for (var i in files) {
-            console.log(files[i]);
+            //console.log(files[i]);
             processFileName(files[i]);
         }
     }
@@ -47,8 +47,6 @@ function parse(fileName) {
         year: arr[1].split(")")[0]
     };
 
-    console.log("in parse: filename: " + fileName + " year: " + movieInput.year);
-
     return movieInput;
 }
 
@@ -61,40 +59,33 @@ function processFileName(fileName) {
 
             var release_date = res.results[i].release_date;
             var release_year = release_date.split("-")[i];
-
-            if (i == 0) {
-                console.log(res.results[0]);
-                console.log("input: " + fileName + " parsed year: " + movieInput.year);
-                console.log("input: " + fileName + " api year: " + release_year);
-            }
+            var movie_id = res.results[i].id;
 
             if (release_year == movieInput.year) {
                 /*
-                 db.movie.save(res.results[i], function(err, saved) {
-                 if( err || !saved ) console.log("Movie not saved");
-                 else console.log("Movie saved");
-                 }); */
-                //res.results[i].fileName = folderPath + "/" + fileName;
+                    Stores a soft link of the movie source into the
+                    static folder in the web server for video streaming
+                */
+                var source = folderPath + "/" + fileName;
+                var dest = 'static/videos/' + res.results[i].id + '.mp4';
+                child = exec('ln -s "' + source + '" "' + dest + '"');
 
                 //db.movie.save(res.results[i]);
-                mdb.movieInfo({id: res.results[i].id},function(err,res){
-                    console.log(res);
+                mdb.movieInfo({id: movie_id },function(err,res){
+                    db.movie.save(res);
+                });
+
+                mdb.movieSimilar({id: movie_id },function(err,res){
                     db.movie.save(res);
                 });
 
 
-
                 //retrieve and store list of available translations for a movie
-                mdb.movieTranslations({id: res.results[i].id},function(err,res){
-                    console.log(res);
+                mdb.movieTranslations({id: movie_id},function(err,res){
                     db.translations.save(res);
                 });
 
-                //db.close()
-                console.log("years are the same for " + fileName);
-
-                mdb.movieReviews({id:res.results[i].id},function(err,res){
-                    console.log(res);
+                mdb.movieReviews({id:movie_id},function(err,res){
                     db.reviews.save(res);
                 });
                 break;
@@ -110,12 +101,13 @@ function getAllGenre() {
             //console.log(res);
             db.genre.save(res.genres[i], function(err, saved){
                 if(err || !saved) {
-                    console.log("Error.Genre not updated in collection!!");
-                    console.log(err);
+                    //console.log("Error.Genre not updated in collection!!");
+                    //console.log(err);
                     db.close();
                 }
-                else
-                    console.log("Genre updated in database");
+                else {
+                    //console.log("Genre updated in database");
+                }
             //db.close
             });
         }
